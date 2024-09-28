d9b14e5a612009cf2b222c9cc454388c899b42c4
Mohamed Azarudeen <azartechnet@gmail.com>
2024-09-28 19:42:04 +0530

    updated

--- Reactjs/bavishbatch/src/index.js
+++ Reactjs/bavishbatch/src/index.js
@@ -15,7 +15,7 @@ r1.render(<Sample/>)*/
 
 //Dynamic Rendering
 
-const App=()=>{
+/*const App=()=>{
   const username="mohamed";
   return(
     <div>
@@ -26,4 +26,66 @@ const App=()=>{
 }
 const r1=ReactDOM.createRoot(document.getElementById('root'));
 
-r1.render(<App/>); 
+r1.render(<App/>); */
+
+//React with JSX
+
+/*const myelm=<h1>Welcome1</h1>
+const r1=ReactDOM.createRoot(document.getElementById('root'));
+r1.render(myelm);
+
+//React without JSX
+
+ const myelm2=React.createElement('h1',null,'Welcome2');
+ const r2=ReactDOM.createRoot(document.getElementById('root'));
+ r2.render(myelm2);*/
+
+ //Expression in React
+ /*const name="mohamed";
+
+ const age=25;
+
+  const App=()=>{
+    return(
+      <div>
+        <h1>Hello {name}</h1>
+        <p>Age:{age}</p>
+      </div>
+      )
+      }
+      const r1=ReactDOM.createRoot(document.getElementById('root'));
+      r1.render(<App/>);*/
+
+      //Fragement tag
+
+      /*const App=()=>{
+        return(
+         <>
+        <h1>Welcome</h1>
+        <h2>Welcome</h2>
+          </>
+        
+        )
+      }
+      const r1=ReactDOM.createRoot(document.getElementById('root'));
+      r1.render(<App/>);*/
+
+      //Importing  CSS in React
+
+      import './index.css';
+
+      const App=()=>{
+        return(
+          <div >
+            <h1 className="sam">Welcome</h1>
+            <h2>Welcome</h2>
+            <form>
+              <label className='form-label'>UserName</label>
+              <input type='text' className='form-control'/>
+              <input type='submit' value='Login' className='btn btn-info'/>
+            </form>
+          </div>
+        )
+      }
+      const r1=ReactDOM.createRoot(document.getElementById('root'));
+      r1.render(<App/>);
