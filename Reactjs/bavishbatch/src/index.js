984917814d33f5d9ca69e7ca746b2b02d8b3f5b4
Mohamed Azarudeen <azartechnet@gmail.com>
2024-09-29 17:45:09 +0530

    Update index.js

--- Reactjs/bavishbatch/src/index.js
+++ Reactjs/bavishbatch/src/index.js
@@ -72,7 +72,7 @@ r1.render(myelm);
 
       //Importing  CSS in React
 
-      import './index.css';
+      /*import './index.css';
 
       const App=()=>{
         return(
@@ -88,4 +88,134 @@ r1.render(myelm);
         )
       }
       const r1=ReactDOM.createRoot(document.getElementById('root'));
-      r1.render(<App/>);
+      r1.render(<App/>);*/
+
+      //Condition Rendering
+
+      /*const x=10;
+      let text="hello";
+      if(x>10)
+      {
+        text="world";
+      }
+      else{
+        text="hello";
+      }
+      console.log(text)
+
+      const Myelem=()=>{
+        return(
+          <div>
+            <h1>{text}Welcome</h1>
+            
+          </div>
+        )
+      }
+      const r1=ReactDOM.createRoot(document.getElementById('root'));
+      r1.render(<Myelem/>);*/
+
+      //Else if ladder
+
+      /*const Gradesystem=()=>{
+        let grade;
+        const stumrks=prompt("Enter the Marks::")
+        if(stumrks>=90)
+          {
+            grade="A"
+            }
+            else if(stumrks>=80)
+              {
+                grade="B"
+                }
+                else if(stumrks>=70)
+                  {
+                    grade="C"
+                    }
+                    else if(stumrks>=60)
+                      {
+                        grade="D"
+                        }
+                        else
+                        {
+                          grade="F"
+                          }
+                          console.log(grade)
+                          return(
+                            <div>
+                              <h1>Grade is {grade}</h1>
+                            </div>
+                            )
+
+      }
+       const r1=ReactDOM.createRoot(document.getElementById('root'));
+       r1.render(<Gradesystem/>)*/
+
+       //Switch  case ladder
+
+       /*const getGrade=(score)=>{
+        switch(true){
+          case score>=90:
+            return 	"A"
+            case score>=80:
+              return "B"
+              case score>=70:
+                return "C"
+                case score>=60:
+                  return "D"
+                  default:
+                    return "F"
+                    }
+                    
+       }
+       const Gradesystem=()=>{
+        const stumrks=85
+        const grade=getGrade(stumrks)
+        console.log(grade)
+       }
+       
+
+       const r1=ReactDOM.createRoot(document.getElementById('root'));
+       r1.render(<Gradesystem/>)*/
+
+       //Class Component
+
+       /*class Sample extends React.Component
+       {
+          render()
+          {
+            return(
+              <div>
+                <h1>Hello</h1>
+              </div>
+            )
+          }
+       }
+       const r1=ReactDOM.createRoot(document.getElementById('root'));
+       r1.render(<Sample/>)*/
+
+       //Function component
+
+       /*function Sample()
+       {
+          return(
+            <div>
+              <h1>Hello</h1>
+            </div>
+          )
+       }
+       const r1=ReactDOM.createRoot(document.getElementById('root'));
+       r1.render(<Sample/>)*/
+
+      //Function Component using props
+
+      function Sample(props)
+      {
+         return(
+          <div>
+            <h1>Hello {props.name}</h1>
+            <h2>Welcome {props.id}</h2>
+          </div>
+         )
+      }
+       const r1=ReactDOM.createRoot(document.getElementById('root'));
+       r1.render(<Sample name="Rahul" id="123"/>)
